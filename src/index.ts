13c243067f5114c4ec32b7410ce82db1f43be09d
James Gee <geemanjs@gmail.com>
2020-05-17 21:58:31 +0100

    fix: try with conventionalcommits preset

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,33 @@
-import * as github from '@actions/github';
 import * as sh from '@actions/exec';
 import * as core from '@actions/core';
-import {getCommitMessages, getRawCommitMessages} from "./commits";
 import conventional from 'conventional-recommended-bump';
 import changelog from 'conventional-changelog-conventionalcommits/conventional-recommended-bump';
-import changelog2 from 'conventional-changelog-conventionalcommits';
+import skipCi from 'skip-ci';
+
+function getRecommendation(): Promise<{ level: number, reason: string, releaseType: 'patch' | "minor" | "major" }> {
+  return new Promise(((resolve, reject) => {
+    conventional({
+      whatBump: changelog({}).whatBump
+    }, (error, recommendation) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(recommendation);
+    })
+  }))
+}
 
 async function run() {
   try {
-    console.log(github.context.sha)
-    console.log(github.context)
-    console.log(changelog);
-    console.log(changelog2)
-
-    const commits = getCommitMessages();
-    // const versionBump = commits.reduce((agg, commit) => {
-    //   commit.type === ""
-    // }, "patch")
-
-    conventional({
-      preset: 'conventional-commits'
-      // whatBump: changelog({}).whatBump
-    }, (error, recommendation) => {
-      console.log(error, recommendation)
-    })
-
-    console.log("----------");
-    console.log(getRawCommitMessages());
-    console.log("----------");
-    console.log();
-    console.log("-----------");
-
-
-    await sh.exec("npm", ["--version"]);
-    await sh.exec("npm config get registry")
+    if (skipCi) {
+      core.setOutput("skip", true);
+      return;
+    }
+    const {releaseType} = await getRecommendation();
+    await sh.exec("npm", ["version", releaseType, "-m", `"[skip ci] Bumping version by ${releaseType}"`]);
+    await sh.exec("npm", ["publish"]);
+    await sh.exec("git", ["push"]);
+    await sh.exec("git", ["push", "--tags"]);
   } catch (err) {
     console.log(err);
     core.setFailed(err.message);
